4c59017d77601f44c595324b233362541ed12965
Harshit Kumar <kharshit020@gmail.com>
2026-01-11 17:21:46 +0530

    bind server to 0.0.0.0

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,9 @@ import app from "./app";
 import "dotenv/config"
 
 console.log(process.env.DATABASE_URL)
-const PORT = process.env.PORT || 4000;
+const PORT = Number(process.env.PORT) || 4000;
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT,"0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
 
