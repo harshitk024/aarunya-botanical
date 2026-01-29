e1bc9d7644fe0ffd8145b16105c2d66987ba9665
Harshit Kumar <kharshit020@gmail.com>
2026-01-29 20:36:22 +0530

    url error fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cookieParser())
 app.use(timeout("10m"))
-app.use(cors({origin: "http://localhost:5173",credentials: true}));
+app.use(cors({origin: "https://www.aarunyabotanicals.com",credentials: true}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
